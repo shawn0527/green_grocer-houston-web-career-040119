f056aa8ce4b53ed2467e27ffb963476ede850748
Shawn Ren <doggy-ren@hotmail.com>
2019-03-23 20:01:03 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,4 +1,15 @@
 def consolidate_cart(cart)
+  result_hash ={}
+  cart.each do |food|
+    food.each do |food, data|
+      if result_hash.has_key?(food) == false
+        result_hash[food] = data
+        result_hash[food][:count] = 1
+      else result_hash[food][:count] += 1
+      end
+    end
+  end
+  result_hash
   # code here
 end
 
